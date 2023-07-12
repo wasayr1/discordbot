fdf206f8e76b03eff93f9b46d8f4c0829336573f
Wasay Rizwan <wasayr1@gmail.com>
2023-07-12 00:07:30 -0400

    Adding bot

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+import discordbot.responses as responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    TOKEN = 'MTEyODA0NTk2NDEzMzkyOTE0Mg.GQNFQ-.4lVQ7LGu64FqcWS0zRxcjgWVdyZSxnG4AGy-b0'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+    
+    client.run(TOKEN)
